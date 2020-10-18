e9d11e74de227b8e5672226dde83c65f03ce7e9a
Jacob <zeitzew32@gmail.com>
2020-10-17 18:56:25 -0700

    Title shift

--- Bloc_Intro/android/app/src/main/kotlin/com/example/Bloc_Intro/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Bloc_Intro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter_Counter/lib/app.dart
+++ Flutter_Counter/lib/app.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'counter/counter.dart';
 
 class CounterApp extends MaterialApp {
-  const CounterApp({Key key}) : super(key:key, home: const CounterPage());
+  const CounterApp({Key key}) : super(key: key, home: const CounterPage());
 }
