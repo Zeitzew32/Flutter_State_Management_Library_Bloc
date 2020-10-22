2c4acc79e1766dc5c865fbd3c2141d6d5bcc4b4c
Jacob <zeitzew32@gmail.com>
2020-10-22 15:13:42 -0700

    Finished Bloc

--- Flutter_Login/lib/authentication/bloc/authentication_event.dart
+++ Flutter_Login/lib/authentication/bloc/authentication_event.dart
@@ -6,3 +6,13 @@ abstract class AuthenticationEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class AuthenticationStatusChanged extends AuthenticationEvent {
+  const AuthenticationStatusChanged(this.status);
+
+  final AuthenticationStatus status;
+
+  List<Object> get props => [status];
+}
+
+class AuthenticationLogoutRequested extends AuthenticationEvent {}
