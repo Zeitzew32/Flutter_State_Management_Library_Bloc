b69673cfdd63d330c22578f6edabc2d04acff53c
Jacob <zeitzew32@gmail.com>
2020-10-23 17:22:45 -0700

    Setup first view

---
+++ Flutter_Weather/lib/widgets/weather.dart
@@ -0,0 +1,79 @@
+import 'package:Flutter_Weather/bloc/weather_bloc.dart';
+import 'package:Flutter_Weather/bloc/weather_state.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:Flutter_Weather/bloc/blocs.dart';
+import 'widgets.dart';
+
+class Weather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Weather'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              final city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CitySelection(),
+                ),
+              );
+              if (city != null) {
+                BlocProvider.of<WeatherBloc>(context)
+                    .add(WeatherRequested(city: city));
+              }
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder<WeatherBloc, WeatherState>(
+          builder: (context, state) {
+            if (state is WeatherInitial) {
+              return Center(child: Text('Please Select a Location'));
+            }
+            if (state is WeatherLoadInProgress) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (state is WeatherLoadSuccess) {
+              final weather = state.weather;
+
+              return ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 100),
+                    child: Center(
+                      child: Location(location: weather.location),
+                    ),
+                  ),
+                  Center(
+                    child: LastUpdated(dateTime: weather.lastUpdated),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 50.0),
+                    child: Center(
+                      child: CombinedWeatherTemperature(
+                        weather: weather,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+            if (state is WeatherLoadFailure) {
+              return Text(
+                'Something Went Wrong!',
+                style: TextStyle(color: Colors.red),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
